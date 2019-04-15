fe93ceb806e80f0e86ea979a6d0d3d96d7a1b5c8
Victor Ayala-Alfaro <39524423+Victor-ayala@users.noreply.github.com>
2019-04-15 10:09:24 -0500

    Update lane_detection.py

--- packages/lane_detection/src/lane_detection.py
+++ packages/lane_detection/src/lane_detection.py
@@ -29,169 +29,195 @@ class laneDetector :
         ##-----------------------------
         reverse = True
         crop = True
-        fraction = 0.5
-        
+        flip = False
+        fraction = 0.35
+
         im_in = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
         width = int(im_in.shape[1])
         height = int(im_in.shape[0])
-        
+
         if reverse:
-            im_in = cv2.bitwise_not(im_in)
-            #cv2.imshow("Not image", im_in)
+        im_in = cv2.bitwise_not(im_in)
         #    cv2.imshow("Not image0", im_in)
-        
+
         # cropping image
         if crop:
             crop_y = int(im_in.shape[0]*(1-fraction))
             cropped_image = im_in[crop_y:height,0:width]
             img = image[crop_y:height,0:width]
-        else:
-            cropped_image = im_in
-            img = image
-        #cv2.imshow("Cropped image", img)
-            
-        pixels = width*height
+            width = int(cropped_image.shape[1])
+            height = int(cropped_image.shape[0])
+            half_img = cropped_image.copy() 
         
+        else:
+            cropped_image = im_in.copy()
+            img = image.copy()
+   
+        for i in range(width/2 - 50):
+            half_img[:, i] = 1 
+    
+        if flip:
+            cropped_image = cv2.flip(cropped_image, 1 )
+            img = cv2.flip(img, 1 )
+
+
+        cv2.imshow("Half image", half_img)
+        cv2.imshow("Cropped image", cropped_image)
+
         pixels = int(img.shape[1]*img.shape[0])
-        maxArea = pixels*10/100                     # 10% of pixels
-        minArea = 20             # 0.01% of pixels
-        
         h_img = int(img.shape[0])
-        
-        ## Bilateral filter
-#        blurred_image = cv2.bilateralFilter(cropped_image, 15, 55, 55)
-        #cv2.imshow("blurred image", blurred_image)
-        
-        #x_image= cv2.adaptiveThreshold(cropped_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
-        #                               cv2.THRESH_BINARY, 11, 2)
-        #cv2.imshow("blurred image", x_image)
-        
-        ## Threshold
-        #th, im_th = cv2.threshold(blurred_image, 250, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        th, im_th = cv2.threshold(cropped_image, 250, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        #th, im_th = cv2.threshold(cropped_image, 250, 255, cv2.THRESH_BINARY)
-#        cv2.imshow("Not image binary", im_th)
-        
-        ## Morphological transformantion
-        ## closing
-        kernel = np.ones((3, 3),np.uint8)
-        im = cv2.morphologyEx(im_th, cv2.MORPH_CLOSE, kernel, iterations = 1)
-        #cv2.imshow("Not image", im)
+
         ##-----------------------------
-        
-        ## Find contours
-        im2, contours, hierarchy = cv2.findContours(im, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        for i in contours:
-            cv2.drawContours(img, i, -1, (0, 0, 255), 3)
-        #cv2.imshow("Cropped image", img)
-        
-        ## Ordered contours
-        contours = sorted(contours, key = cv2.contourArea, reverse = True)
-        
-        contours_ord = []
-#        bl_contours = []
-        big_blob = []
-        # Draw centroids
-        for i in contours:
-            area = cv2.contourArea(i)
-            if area > minArea and area < maxArea:
-                contours_ord.append(i)
-                cv2.drawContours(img, i, -1, (0, 0, 255), 3)
-                
-        if len(contours_ord) > 0:   
-            # Obtain biggest blob (continuous line)
-            big_blob.append(contours_ord[0])
-            _, _, big_blob_centroid = my.obtainCentroid(big_blob, minArea, maxArea)
-            cv2.drawContours(img, big_blob, -1, (0, 200 ,200), 3)
-            
-            cx_array, cy_array, centroids = my.obtainCentroid(contours_ord, minArea, maxArea)
-            
-            ## Sorting centroids
-            centroids = sorted(centroids, key = operator.itemgetter(0), reverse=True)
-            
-            centroids_srt = []
-            condition = False
-            
-            ## Pop blobs on the right of continuous line
+
+
+        ## Setup SimpleBlobDetector parameters.
+        params = cv2.SimpleBlobDetector_Params()
+ 
+        maxArea = 1*pixels/100                  # 1% of cells
+        minArea = 0.05*pixels/100               # 0.02% of cells
+        #maxArea = 5*pixels/100                  # 5% of cells
+        #minArea = 0.5*pixels/100               # 0.5% of cells
+
+        ## Change thresholds
+        params.filterByColor = True
+        params.blobColor = 255
+        params.minThreshold = 180
+        params.maxThreshold = 255
+ 
+        ## Filter by Area.
+        params.filterByArea = True
+        params.minArea = minArea
+        params.maxArea = maxArea
+ 
+        ## Filter by Circularity
+        params.filterByCircularity = False
+        #params.minCircularity = 0.1
+ 
+        ## Filter by Convexity
+        params.filterByConvexity = True
+        params.minConvexity = 0.7
+ 
+        ## Filter by Inertia
+        params.filterByInertia = False
+        params.minInertiaRatio = 0.0
+        params.maxInertiaRatio = 0.2 
+
+        ## Create a detector with the parameters
+        detector = cv2.SimpleBlobDetector_create(params)
+
+        # Detect blobs.
+        dashedline = detector.detect(cropped_image)
+
+        ## Setup new detection parameters
+        params2 = cv2.SimpleBlobDetector_Params()
+ 
+        maxArea = 4000            
+        minArea = 400               
+
+        ## Change thresholds
+        params2.filterByColor = True
+        params2.blobColor = 255
+        params2.minThreshold = 100
+        params2.maxThreshold = 255
+ 
+        ## Filter by Area.
+        params2.filterByArea = True
+        params2.minArea = minArea
+        params2.maxArea = maxArea
+ 
+        ## Filter by Circularity
+        params2.filterByCircularity = False
+        #params.minCircularity = 0.1
+ 
+        ## Filter by Convexity
+        params2.filterByConvexity = True
+        params2.minConvexity = 0.7
+ 
+        ## Filter by Inertia
+        params2.filterByInertia = False
+        params2.minInertiaRatio = 0.0
+        params2.maxInertiaRatio = 0.2 
+
+        detector2 = cv2.SimpleBlobDetector_create(params2)
+        contline = detector2.detect(half_img)
+
+        #for x in contline:
+        #        print int(x.pt[0]), int(x.pt[1])
+
+        ## Check if there are lines to detect
+        if len(dashedline) > 2 and len(contline) > 0:
+            ## Sort big blobs by 
+            cont = []
+            for x in contline:
+                cont.append((int(x.pt[0]), int(x.pt[1])))
+            cont = sorted(cont, key = operator.itemgetter(1), reverse = True)
+    
+            pc = cont[0]
+    
+            centroids = []
+            for x in dashedline:
+                centroids.append((int(x.pt[0]), int(x.pt[1])))
+        #        cv2.circle(img, (int(x.pt[0]), int(x.pt[1])), 8, (0, 255, 189), 3)
+        #    print centroids
+    
+            rangem = 0.2
+            x, y, centroids, dashedline = my.checkSlope(centroids, dashedline, rangem)
+    
+            m, b = my.linearRegression(x, y)
+            pi, pf = my.getDashedLine(m, b, h_img)
+    
+
+            yd = pc[1]
+            xd = int((yd - b)/m)
+            pd = (xd, yd)
+    
+            center = my.obtainLaneCenter(pd, pc)
+    
+            #Draw points and line found
             for i in centroids:
-                if i == big_blob_centroid[0]:
-                    condition = True
-                if condition:
-                    centroids_srt.append(i)
+                cv2.circle(img, i, 8, (0, 255, 189), 3)
+            cv2.line(img, (pi[0], pi[1]), (pf[0], pf[1]), (0, 200, 200), thickness=4)
+            cv2.line(img, (pd[0], pd[1]), (pc[0], pc[1]), (250, 200, 0), thickness=4)
+            cv2.circle(img, pc, 10, (250, 100, 0), 4)
+            cv2.circle(img, pd, 10, (250, 100, 0), 4)
+            cv2.circle(img, center, 10, (200, 200, 0), 4)
+    
+            ## Controller 
+            theta_m = 30                                # deg   min/max value 
+            w_lane = 40.0                               # cm    lane width
+            k = float(w_lane/float(pc[0] - pd[0]))      # Conversion from pixels to cm
+            kv = 0.01
+            d_offset = 20                               # cm    from car to bottom image
+            d = kv*(height - center[1])                 # cm    from bottom to calculated point
+    
+            pcar = (width/2, int(height/2))
+            error = k*(center[0] - pcar[0])
+            angle = np.arctan2(error , d + d_offset)*(180/np.pi)
+            if angle > theta_m:
+                angle = theta_m
+            if angle < -theta_m:
+                angle = -theta_m
         
-            if len(centroids_srt) > 2:
-            
-                ## Center point of continuous line
-                x_c = centroids_srt[0][0]                    
-                y_c = centroids_srt[0][1]
-                ## Exclude most right centroid (continuous line)
-                centroids_srt.pop(0)
-                
-                ## Re-crop image
-                max_y = max(centroids, key = operator.itemgetter(1))
-                new_cropped_image = img[0:max_y[1], 0:width]
-                #cv2.imshow("New cropped image", new_cropped_image)
-            
-                centroids_srt = sorted(centroids_srt, key = operator.itemgetter(1), reverse=True)
-            
-                ## Preparing centroids for linear regresion
-                x = []
-                y = []
-                for i in centroids_srt:
-                    x.append(i[0])
-                    y.append(i[1])
-                #print(len(x))
-                ## Check slope to discard false blob in dashed line
-                #a, b, centroids = my.checkSlope(x, y, centroids)
-            
-                ## Obtain slopes to compare between them
-                m_array = []
-                for i in range(1, len(x)):
-                    slope = (y[i] - y[i - 1])/(x[i] - x[i - 1])
-                    m_array.append(slope)
-            
-                ## Search the  slope mode
-                mode = max(set(m_array), key=m_array.count)
-                xpop = []
-                for i in range(1, len(centroids_srt)):
-                    if m_array[i - 1] != mode:
-                        xpop.append(i)
-                    else: 
-                        cv2.circle(img, (x[i], y[i]) , 10, (0, 200, 0), 4)
-                
-                ## Pop blacklisted centroids
-                for i in reversed(xpop):
-                    centroids_srt.pop(i)
-                    x.pop(i)
-                    y.pop(i)
-            
-                ## Linear regresion procedure
-                m, b, x_d, y_d, x_m, y_m = my.linearRegression(x, y, h_img)
-                #cv2.circle(img, (0, int(b)) , 10, (200, 200, 0), 4)
-                
-                y_t = [b, 0]
-                y_t = np.array(y_t, dtype=np.float64)
-#                x_t = (y_t - b)/m
-                    
-                y_d = int(y_c)                    
-                x_d = int((y_d - b)/m)
-                
-                # Obtain car center
-                x_car, y_car = my.obtainCarCenter(x_d, y_d, x_c, y_c)
-                
-                # Draw obtained lines and point
-                cv2.line(img, (int(x_m[0]), int(y_m[0])), (int(x_m[-1]), int(y_m[-1])), (255, 0, 0), thickness = 4)
-                cv2.line(img, (x_d, y_d), (x_c, y_c), (0, 200, 200), thickness=4)
-                cv2.circle(img, (x_car, y_car), 8, (0, 255, 189), 3)
-                #cv2.circle(img,(x_c,y_c), 10, (0, 200, 0), 4)
-                #cv2.circle(img,(x_d, y_d), 10, (0, 200, 0), 4)
-          
-            else:
-                print "Not enough points on dashed line"
-                Success = False
+            ## Conversion to int steering value
+            steer_range = 170                   # 5 - 175
+            theta_range = 60                    # -30 - 30
+            steer = int(steer_range/theta_range * angle + 90)
+
         else:
-            print "There's no blobs to detect"
-            Success = False
+            steer = 90
+            print "There are no blobs to detect"
+
+        print steer
+        print angle
+
+        # Draw detected blobs as red circles.
+        # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+        # img = cv2.drawKeypoints(img, contline, np.array([]), (255, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        # img = cv2.drawKeypoints(img, dashedline, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+ 
+        # Show keypoints
+        cv2.imshow("Lane detection", img)
+        cv2.waitKey(0)
 
         return Success, img
 
